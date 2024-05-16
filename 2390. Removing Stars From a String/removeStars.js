2de98a3953f8f3ab193eedff5331355a9e958156
julesdmai <julesdmai@gmail.com>
2024-05-16 13:30:36 -0700

    removeStars solution

---
+++ 2390. Removing Stars From a String/removeStars.js
@@ -0,0 +1,34 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var removeStars = function(s) {
+    if (!s) return null;
+    
+    // Initialize variables
+    let modifiedS = '';
+    let arrayOfChars = [];
+
+    // Iterate through chars
+    for (let i = 0; i < s.length; i++) {
+        let thisChar = s[i];
+        let nextChar = s[i + 1];
+
+        if (thisChar !== '*') {
+            arrayOfChars.push(thisChar); // We collect the char as long as the char is not a *
+        }
+        
+        if (nextChar === '*') {
+            arrayOfChars.pop(); // However, if the next char is a *, then we must remove a letter
+        }
+    }
+    
+    return (arrayOfChars.join(""));
+    
+};
+
+// // Testing
+// s1 = "leet**cod*e" // Output: "lecoe"
+// console.log(removeStars(s1));
+// s2 = "erase*****" // Output: ""
+// console.log(removeStars(s2));
