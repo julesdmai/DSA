e397fbb192dc79ae92d49033ace311588dc83ac5
julesdmai <julesdmai@gmail.com>
2024-05-21 16:30:32 -0700

    equalPairs solution

--- 2352. Equal Row and Column Pairs/equalPairs.js
+++ 2352. Equal Row and Column Pairs/equalPairs.js
@@ -4,41 +4,45 @@
  */
 // O(n) // spaceO(n)
 var equalPairs = function(grid) {
-    const cache = {};
-    let value = '';
+    const rowCache = {};
+    const columnCache = {};
 
-    for (let row = 0; row < grid.length; row++) {
-        for (let column = 0; column < grid.length; column++) {
-            value += `${grid[row][column]}`;
-        }
-        cache[value] = (cache[value] || 0) + 1; // Add to cache
-        value = ''; // Reset string
+    // Helper function to convert array to string to serve as a key
+    const arrayToString = (array) => {
+        return array.join(',');
     }
 
+    // Store grid rows arrays into memory
     for (let row = 0; row < grid.length; row++) {
-        for (let column = 0; column < grid.length; column++) {
-            value += `${grid[column][row]}`;
-        }
-        cache[value] = (cache[value] || 0) + 1; // Add to cache
-        value = ''; // Reset string
+        const rowString = arrayToString(grid[row]);
+        rowCache[rowString] = (rowCache[rowString] || 0) + 1;
     }
 
-    console.log(cache);
+    // Store grid column arrays into memory
+    for (let column = 0; column < grid.length; column++) {
+        let columnsArray = [];
+        for (let row = 0; row < grid.length; row++) {
+            columnsArray.push(grid[row][column]);
+        }
+        
+        // Convert array to string and store in cache
+        const columnString = arrayToString(columnsArray);
+        columnCache[columnString] = (columnCache[columnString] || 0) + 1;
+    }
 
-    const values = Object.values(cache);
+    // Count the number of occurences
     let counter = 0;
-    values.forEach((element) => {
-        if (element > 1) {
-            counter += element - 1;
+    for (const rowString in rowCache) {
+        if (columnCache[rowString]) {
+            counter += rowCache[rowString] * columnCache[rowString];
         }
-    })
-
+    }
     return counter;
 };
 
-// // testing
+// // // testing
 // grid = [[3,2,1],[1,7,6],[2,7,7]];
 // console.log(equalPairs(grid));
 
-grid2 = [[3,1,2,2],[1,4,4,5],[2,4,2,2],[2,4,2,2]];
-console.log(equalPairs(grid2));
+// grid2 = [[3,1,2,2],[1,4,4,5],[2,4,2,2],[2,4,2,2]];
+// console.log(equalPairs(grid2));
