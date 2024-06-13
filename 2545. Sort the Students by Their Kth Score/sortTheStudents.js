cd30c1070d0842c69205f9a20f239a8c0d81b385
julesdmai <julesdmai@gmail.com>
2024-06-13 13:30:55 -0700

    sortTheStudents initial solution

---
+++ 2545. Sort the Students by Their Kth Score/sortTheStudents.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[][]} score
+ * @param {number} k
+ * @return {number[][]}
+ */
+var sortTheStudents = function(score, k) {
+    return score.sort((a, b) => b[k] - a[k]);
+};
+
+
+// Testing
+console.log(sortTheStudents([[10,6,9,1],[7,5,11,2],[4,8,3,15]], 2))
+console.log(sortTheStudents([[3,4],[5,6]], 0))
