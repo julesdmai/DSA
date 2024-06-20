449a25d69eb6eeb6aa0a3822f887e132ff7003a2
julesdmai <julesdmai@gmail.com>
2024-06-20 14:22:29 -0700

    countSubarrays initial solution O(n^2)

---
+++ 2962. Count Subarrays Where Max Element Appears at Least K Times/countSubarrays.js
@@ -0,0 +1,42 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+
+// Strategy: Sliding window
+// O(n^2) // spaceO(1)
+// Input: Array of numbers, and a frequencyCount
+// Output: Number representing count of subarrays
+var countSubarrays = function(nums, k) {
+    let result = 0;
+
+    // Calculate maxValue
+    const frequencyMap = {};
+    for (let num of nums) {
+        frequencyMap[num] = (frequencyMap[num] || 0) + 1;
+    };
+    const numbers = Object.keys(frequencyMap).map(Number);
+    const maxValue = Math.max(... numbers);
+    
+    // Calculate number of subarrays that contain maxValue at least k times
+    for (let start = 0; start < nums.length; start++) {
+        let count = 0;
+
+        for (let end = start; end < nums.length; end++) {
+            if (nums[end] === maxValue) {
+                count++;
+            }
+
+            if (count >= k) {
+                result += (nums.length - end); // Count all subarrays ending at 'end' or after
+                break; // No need to check further as all subsequent subarrays will also be valid
+            }
+        }
+    }
+
+    return result;
+};
+
+// testing
+console.log(countSubarrays([0,0,1,2], 2));
