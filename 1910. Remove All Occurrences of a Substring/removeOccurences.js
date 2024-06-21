4b59ecd0a2c98b51fbde2d4c5f347e0dcaeb10c5
julesdmai <julesdmai@gmail.com>
2024-06-21 11:27:05 -0700

    removeOccurences 2nd solution Stack

--- 1910. Remove All Occurrences of a Substring/removeOccurences.js
+++ 1910. Remove All Occurrences of a Substring/removeOccurences.js
@@ -4,16 +4,31 @@
  * @return {string}
  */
 
-// Strategy: Use method .includes() to match pattern, then use method .replace() to delete the substring
-// O(n^2) // spaceO(n^2)
+// Strategy: Use a stack, if top of stack matches part, then pop off x 3
+// O(n) // spaceO(n)
 // Input: String, string representing pattern
 // Output: Modified string
 var removeOccurrences = function(s, part) {
-    while (s.includes(part)) {
-        s = s.replace(part, '');
+    const stack = [];
+    let partLength = part.length;
+
+    for (let char of s) {
+        stack.push(char);
+
+        // Check if top of stack matches part
+        if (stack.length >= partLength) {
+            let topStack = stack.slice(stack.length - partLength).join('');
+            // Matches, remove the top elements
+            if (topStack === part) {
+                for (let i = 0; i < topStack.length; i++) {
+                    stack.pop();
+                }
+            }
+        }
     }
-    return s;
+
+    return stack.join('');
 };
 
 // // testing
-// console.log(removeOccurrences("daabcbaabcbc", "abc"));
+console.log(removeOccurrences("daabcbaabcbc", "abc"));
