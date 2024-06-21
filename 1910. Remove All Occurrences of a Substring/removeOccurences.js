1b0638dc0849a3c861c3ba3185da9280c0eff38c
julesdmai <julesdmai@gmail.com>
2024-06-21 11:16:48 -0700

    removeOccurences initial solution

---
+++ 1910. Remove All Occurrences of a Substring/removeOccurences.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s
+ * @param {string} part
+ * @return {string}
+ */
+
+// Strategy: Use method .includes() to match pattern, then use method .replace() to delete the substring
+// O(n^2) // spaceO(n^2)
+// Input: String, string representing pattern
+// Output: Modified string
+var removeOccurrences = function(s, part) {
+    while (s.includes(part)) {
+        s = s.replace(part, '');
+    }
+    return s;
+};
+
+// // testing
+// console.log(removeOccurrences("daabcbaabcbc", "abc"));
