8de0ed3cfc0eeca76e48c74d492bb058d641cbb7
julesdmai <julesdmai@gmail.com>
2024-07-02 21:41:46 -0700

    maxNumOfMarkedIndices work in progress

---
+++ 2576. Find the Maximum Number of Marked Indices/maxNumOfMarkedIndices.js
@@ -0,0 +1,49 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+// Strategy: Sort array, initialize i and j pointers to the beginning of array, create a set for marked indices, iterate through array, check for criteria, return set size
+// O(nlogn) // spaceO(n)
+// Input: Array of numbers
+// Output: Number representing marked set size
+var maxNumOfMarkedIndices = function(nums) {
+    // Sort array
+    nums.sort((a, b) => a - b);
+
+    // Initialize i and j pointers to beginning of array
+    let i = 0;
+    let j = 1;
+
+    // Create a set for marked indices
+    const markedIndices = new Set();
+
+    // Itereate through array
+    while (j <= nums.length - 1) {
+        // Check for criteria
+        if (2 * nums[i] < nums[j]) {
+            // Push indices to set
+            markedIndices.add(i);
+            markedIndices.add(j);
+
+            // Re-assign pointers
+            while (markedIndices.has(i)) {
+                i++;
+            }
+            j = i + 1;
+            while (markedIndices.has(j)) {
+                j++;
+            }
+        } else {
+            j++;
+        }
+    }
+    
+    // End loop, return set size
+    return markedIndices.size;
+};
+
+// // Testing
+// console.log(maxNumOfMarkedIndices([3,5,2,4]));
+// console.log(maxNumOfMarkedIndices([9,2,5,4]));
+// console.log(maxNumOfMarkedIndices([7,6,8]));
