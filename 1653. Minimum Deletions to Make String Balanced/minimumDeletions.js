214c1da990f2ae538191e458dd7d511f6df89dcc
julesdmai <julesdmai@gmail.com>
2024-07-08 18:46:27 -0700

    minimumDeletions solution

--- 1653. Minimum Deletions to Make String Balanced/minimumDeletions.js
+++ 1653. Minimum Deletions to Make String Balanced/minimumDeletions.js
@@ -3,23 +3,28 @@
  * @return {number}
  */
 
-// Strategy: Two counters for countA and minDeletions, iterate through the string, deal with encountering 'a' and 'b', update minDeletions
-// O(n) // spaceO(1)
+// Strategy: Stack for keeping track, if current letter is less than top of stack, then pop off stack and increment deleteCount
+// O(n) // spaceO(n)
 // Input: String
 // Output: Number representing minimum deletions
 var minimumDeletions = function(s) {
-    let countA = 0;
-    let minDeletions = 0;
+    const stack = [];
+    let deleteCount = 0;
 
     for (let char of s) {
-        if (char === 'a') countA++;
-        else { // encountered a 'b'
-            minDeletions = Math.min(minDeletions + 1, countA);
+        let topOfStack = stack[stack.length - 1];
+
+        if (stack.length > 0 && char < topOfStack) { // balancing required
+            stack.pop();
+            deleteCount++;
+        } else { // no balancing required
+            stack.push(char);
         }
     }
 
-    return minDeletions;
+    return deleteCount;
 };
 
 // // testing
 // console.log(minimumDeletions('aababbab'))
+// console.log(minimumDeletions('bbaaaaabb'));
