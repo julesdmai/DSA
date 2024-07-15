6f1bd93499ade8517e21670688b6936093d28c1d
julesdmai <julesdmai@gmail.com>
2024-07-15 12:58:31 -0700

    calculate strategy

--- 227. Basic Calculator II/calculate.js
+++ 227. Basic Calculator II/calculate.js
@@ -2,6 +2,11 @@
  * @param {string} s
  * @return {number}
  */
+
+// Strategy: Two stacks to hold numbers and operations, first pass to handle mult/div and second pass to handle add/sub
+// O(n) // spaceO(n)
+// Input: String
+// Output: Number
 var calculate = function(s) {
     
 };
