fb95d142550ebd68cf376b4cb899720d133a3b9e
julesdmai <julesdmai@gmail.com>
2024-07-17 20:23:13 -0700

    removeDuplicateLetters solution

--- 316. Remove Duplicate Letters/removeDuplicateLetters.js
+++ 316. Remove Duplicate Letters/removeDuplicateLetters.js
@@ -3,23 +3,37 @@
  * @return {string}
  */
 
-// Strategy: Split chars into elements in an array, form a set from the array removing duplicates, form an array back from the set, sort the array, join and return
-// O(nlogn) // spaceO(n)
+// Strategy: Find lastOccurence of each char in string s, iterate through s, construct a stack that fits the criteria, use a set to keep track of which chars have been added to the set, join and return
+// O(n) // spaceO(n)
 // Input: string
 // Output: string
 var removeDuplicateLetters = function(s) {
-    // Set-wash to remove duplicates
-    const arrayS = s.split('');
-    const setS = new Set(arrayS);
-    const arraySNoDuplicates = Array.from(setS);
+    // Find last occurence of each character in the string
+    const lastOccurence = {};
+    for (let i = 0; i < s.length; i++) {
+        lastOccurence[s[i]] = i;
+    }
 
-    // Sort array
-    arraySNoDuplicates.sort();
+    const stack = [];
+    const seen = new Set();
 
-    // Join and return
-    return arraySNoDuplicates.join('');
+    // Iterate through s constructing stack according to prompt
+    for (let i = 0; i < s.length; i++) {
+        let thisChar = s[i];
+        if (seen.has(thisChar)) continue;
+
+        while (stack.length && stack[stack.length - 1] > thisChar && lastOccurence[stack[stack.length - 1]] > i) {
+            seen.delete(stack.pop());
+        }
+
+        stack.push(thisChar);
+        seen.add(thisChar);
+    }
+
+    return stack.join('');
 };
 
 // testing
 // console.log(removeDuplicateLetters('bcabc'));
 // console.log(removeDuplicateLetters('cbacdcbc'));
+// console.log(removeDuplicateLetters('ecbacba'));
