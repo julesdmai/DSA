8052893a501f75964feea0f90dd7770afb18413f
julesdmai <julesdmai@gmail.com>
2024-07-21 17:04:19 -0700

    deepestLeaves 2nd solution

--- 1302. Deepest Leaves Sum/deepestLeavesSum2.js
+++ 1302. Deepest Leaves Sum/deepestLeavesSum2.js
@@ -11,29 +11,31 @@
  * @return {number}
  */
 
-// Strategy: Level-order traversal while keeping history of prevLevel
+// Strategy: Level-order traversal while keeping track of the sum of current level
 // O(n) // spaceO(n)
 // Input: Root
 // Output: Number
 var deepestLeavesSum = function(root) {
+    if (!root) return 0;
+
     const queue = [root];
-    const prevLevel = [];
+    let currentSum = 0;
+    
     while (queue.length) {
-        // Process nodes from the queue, adding children nodes to the queue
-        for (let i = 0; i < queue.length; i++) {
+        let levelSize = queue.length; // Reset for each new level 
+        currentSum = 0; // Reset for each new level
+
+        for (let i = 0; i < levelSize; i++) {
             let node = queue.shift();
+            currentSum += node.val;
+
             if (node.left) queue.push(node.left);
             if (node.right) queue.push(node.right);
-            prevLevel.push(node);
-        }
-        // If queue has no length, then prevLevel is the deepest nodes 
-        if (queue.length === 0) {
-            break;
-        } else { // Deeper nodes exist, so we reset current history
-            while (prevLevel.length) prevLevel.pop();
         }
     }
 
-    // Calculate sum of nodes
-    return prevLevel.reduce((acc, curr) => acc + curr.val, 0);
+    return currentSum;
 };
+
+// // testing
+// console.log(deepestLeavesSum([1,2,3,4,5,null,6,7,null,null,null,null,8]));
