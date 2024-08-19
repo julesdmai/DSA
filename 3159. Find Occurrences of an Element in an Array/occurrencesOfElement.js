7e44f59acd739555f3f279fa916eb970803a6039
julesdmai <julesdmai@gmail.com>
2024-08-19 09:57:50 -0700

    added comments

--- 3159. Find Occurrences of an Element in an Array/occurrencesOfElement.js
+++ 3159. Find Occurrences of an Element in an Array/occurrencesOfElement.js
@@ -4,6 +4,11 @@
  * @param {number} x
  * @return {number[]}
  */
+
+// Strategy: 
+// O() // spaceO()
+// Input: Array of numbers, array of array of numbers, number
+// Output: Arrayof numbers
 var occurrencesOfElement = function(nums, queries, x) {
     
 };
