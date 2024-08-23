8251ee9d83cc09a1a0522872f1341f4b152298dd
julesdmai <julesdmai@gmail.com>
2024-08-22 17:05:11 -0700

    sumEvenGrandparent initial solution

--- 1315. Sum of Nodes with Even-Valued Grandparent/sumEvenGrandparent.js
+++ 1315. Sum of Nodes with Even-Valued Grandparent/sumEvenGrandparent.js
@@ -10,10 +10,43 @@
  * @param {TreeNode} root
  * @return {number}
  */
-// Strategy: 
-// O() // spaceO()
+// Strategy: Helper function to addGrandchildren, traverse through BST looking for even-valued nodes
+// O(n) // spaceO(n)
 // Input: Root
 // Output: Number
 var sumEvenGrandparent = function(root) {
-    
+    // Helper function to add grandchildren nodes
+    const addGrandchildren = (node, array) => {
+        if (node.left) {
+            if (node.left.left) array.push(node.left.left.val);
+            if (node.left.right) array.push(node.left.right.val);
+        }
+        if (node.right) {
+            if (node.right.left) array.push(node.right.left.val);
+            if (node.right.right) array.push(node.right.right.val);
+        }
+    }
+
+    const targetedValues = [];
+
+    // Recursive traversal of BST
+    const process = (node, array) => {
+        if (!node) return;
+
+        if (node.val % 2 === 0) { // this is an even-valued grandparent
+            addGrandchildren(node, array);
+        }
+
+        // Recursively call to traverse
+        process(node.left, array);
+        process(node.right, array);
+    }
+
+    process(root, targetedValues);
+
+    // Sum targeted values
+    return targetedValues.reduce((acc, curr) => acc + curr, 0);
 };
+
+// // testing
+// console.log(sumEvenGrandparent([6,7,8,2,7,1,3,9,null,1,4,null,null,null,5]));
