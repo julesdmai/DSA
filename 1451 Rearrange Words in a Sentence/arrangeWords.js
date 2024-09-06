7a68df3ecb36607de6940d919bd1be205106f1ef
julesdmai <julesdmai@gmail.com>
2024-09-05 18:08:58 -0700

    wip arrangeWords

--- 1451 Rearrange Words in a Sentence/arrangeWords.js
+++ 1451 Rearrange Words in a Sentence/arrangeWords.js
@@ -3,10 +3,18 @@
  * @return {string}
  */
 
-// Strategy: 
+// Strategy: Lowercase first word, split into elements in array, order by increasing length, capitalize first word, join and return
 // O(n) // spaceO(n)
 // Input: String
 // Output: String
 var arrangeWords = function(text) {
-    
+    // Lowercase first word
+
+    // Split words into array
+
+    // Re-order by increasing length
+
+    // Capitalie first word
+
+    // Join and return
 };
