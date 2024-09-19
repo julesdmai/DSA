5958d224b637026691a417d2c9661dbfcf30e5bd
julesdmai <julesdmai@gmail.com>
2024-09-19 15:03:20 -0700

    Merged two steps of creating tokens and sorting

--- 1451 Rearrange Words in a Sentence/arrangeWords.js
+++ 1451 Rearrange Words in a Sentence/arrangeWords.js
@@ -14,10 +14,8 @@ var arrangeWords = function(text) {
     const lowerCaseFirstLetter = text[0].toLowerCase();
     const newText = lowerCaseFirstLetter + text.slice(1);
 
-    // Split words into separate elements in array
+    // Split words into separate elements in array and re-order by increasing length
     const tokens = newText.split(' ');
-
-    // Re-order words by increasing length
     tokens.sort((a, b) => a.length - b.length);
 
     // Capitalize first letter
