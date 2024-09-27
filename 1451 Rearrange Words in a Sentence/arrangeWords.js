8f5ad0b6303a52c761d43b74eae46b51ca961040
julesdmai <julesdmai@gmail.com>
2024-09-27 15:10:48 -0700

    explicitly made clear that we are creating a clone, not mutating

--- 1451 Rearrange Words in a Sentence/arrangeWords.js
+++ 1451 Rearrange Words in a Sentence/arrangeWords.js
@@ -8,7 +8,7 @@
 // Input: String
 // Output: String
 var arrangeWords = function(text) {
-    // Replace first char in string with lowercase letter
+    // Create new string, replacing the first char with lowercase letter
     const lowerCaseFirstLetter = text[0].toLowerCase();
     const newText = lowerCaseFirstLetter + text.slice(1);
 
