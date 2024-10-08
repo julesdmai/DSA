1eb346363fa5b460ad1e921988304a08db8371dd
julesdmai <julesdmai@gmail.com>
2024-10-08 14:48:28 -0700

    work in progress

--- 1451 Rearrange Words in a Sentence/arrangeWords2.js
+++ 1451 Rearrange Words in a Sentence/arrangeWords2.js
@@ -3,26 +3,12 @@
  * @return {string}
  */
 
-// Strategy: Lowercase first word, split into elements in array, order by increasing length, capitalize first word, join and return, NOTE strings are immutable!
+// Strategy: 
 // O(nlogn) for sorting // spaceO(n)
 // Input: String
 // Output: String
 var arrangeWords = function(text) {
-    // Copy input text to new string, replacing first char with lowercase
-    const lowerCaseFirstLetter = text[0].toLowerCase();
-    const newText = lowerCaseFirstLetter + text.slice(1);
-
-    // Split words into array and re-order by increasing length
-    const tokens = newText.split(' ');
-    tokens.sort((a, b) => a.length - b.length);
-
-    // Copy string and capitalize first letter
-    const capitalFirstLetter = tokens[0][0].toUpperCase();
-    tokens[0] = capitalFirstLetter + tokens[0].slice(1);
-
-    // Join words and return string
-    const results = tokens.join(' ');
-    return results;
+    
 };
 
 // // testing
