218cd807ae99599b363a2864650be7b886f1c6dc
julesdmai <julesdmai@gmail.com>
2024-11-14 13:28:39 -0800

    reverseVowels 2nd solution submission

--- 345. Reverse Vowels of a String/reverseVowels2.js
+++ 345. Reverse Vowels of a String/reverseVowels2.js
@@ -2,6 +2,34 @@
  * @param {string} s
  * @return {string}
  */
+// Input: String
+// Output: String
+// O(n) // spaceO(n)
 var reverseVowels = function(s) {
+    if (!s) return;
+
+    const VOWELS = [
+        'a', 'e', 'i', 'o', 'u',
+        'A', 'E', 'I', 'O', 'U'
+    ]
+
+    const results = [];
+    const vowelsFound = [];
     
+    for (let char of s) {
+        if (VOWELS.includes(char)) vowelsFound.push(char);
+    }
+    
+    for (let char of s) {
+        if (VOWELS.includes(char)) {
+            results.push(vowelsFound.pop());
+        } else {
+            results.push(char);
+        }
+    }
+
+    return results.join('');
 };
+
+// testing
+// console.log(reverseVowels("IceCreAm")) // "AceCreIm"
