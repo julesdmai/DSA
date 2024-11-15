1be96dff5ad402b46dd64b61b3bd9e5b8d86cdb4
julesdmai <julesdmai@gmail.com>
2024-11-15 11:33:56 -0800

    minimumOperations solution

--- 3190. Find Minimum Operations to Make All Elements Divisible by Three/minimumOperations.js
+++ 3190. Find Minimum Operations to Make All Elements Divisible by Three/minimumOperations.js
@@ -5,9 +5,13 @@
 // Input: Array of numbers
 // Output: Number representing minimum operations to make all numbers in array divisble by 3
 // O(n) // spaceO(1)
-// []
+// Assume every number is one operation away from being divisble by 3 -> 7 - 1 = 6, 152 + 1 = 153, 1832+1 = 1833, therefore we just need to check if the number is already divisble by 3, if not then increment a counter
 var minimumOperations = function(nums) {
-    
+    let minOperations = 0;
+    for (let num of nums) {
+        if (num % 3 !== 0) minOperations++;
+    }
+    return minOperations;
 };
 
 // // testing
