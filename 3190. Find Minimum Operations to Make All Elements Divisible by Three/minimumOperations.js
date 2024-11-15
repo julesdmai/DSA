e888b8d433983f42b6615bf3ce6a696eb55915e5
julesdmai <julesdmai@gmail.com>
2024-11-15 11:25:25 -0800

    minimumOperations setup

---
+++ 3190. Find Minimum Operations to Make All Elements Divisible by Three/minimumOperations.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+// Input: Array of numbers
+// Output: Number representing minimum operations to make all numbers in array divisble by 3
+// O(n) // spaceO(1)
+// []
+var minimumOperations = function(nums) {
+    
+};
+
+// // testing
+// console.log(minimumOperations([1,2,3,4]))
+// console.log(minimumOperations([3,6,9]))
