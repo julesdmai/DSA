399bc425bdfc3b3231599462804df408555a9f82
julesdmai <julesdmai@gmail.com>
2024-11-18 11:12:00 -0800

    mostWordsFound start

---
+++ 2114. Maximum Number of Words Found in Sentences/mostWordsFound.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string[]} sentences
+ * @return {number}
+ */
+// Input: Array of strings, where each element represents a sentence
+// Output: Number representing number of words in sentence with most words
+// O(n) // spaceO(1)
+// Strategy: Iterate through array, split string into words, count number of words, keep record of max, return max
+var mostWordsFound = function(sentences) {
+    
+};
