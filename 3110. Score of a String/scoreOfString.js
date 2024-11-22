bf394eaf9c9d1b14ab63c448b49359761f83c981
julesdmai <julesdmai@gmail.com>
2024-11-21 16:41:33 -0800

    Notes added

--- 3110. Score of a String/scoreOfString.js
+++ 3110. Score of a String/scoreOfString.js
@@ -2,7 +2,10 @@
  * @param {string} s
  * @return {number}
  */
-// 
+// Input: String
+// Output: A single number representing the accumulated value
+// O(n) // spaceO(1)
+// Strategy: Reduce method to accumulate a single value
 var scoreOfString = function(s) {
     
 };
