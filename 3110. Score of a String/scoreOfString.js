5e746ee726aeac8eb2b8b2fc0192ede7c422e887
julesdmai <julesdmai@gmail.com>
2024-11-22 17:58:02 -0800

    scoreOfString solution 1

--- 3110. Score of a String/scoreOfString.js
+++ 3110. Score of a String/scoreOfString.js
@@ -7,5 +7,19 @@
 // O(n) // spaceO(1)
 // Strategy: Reduce method to accumulate a single value // OR counter and loop
 var scoreOfString = function(s) {
-    
+    if (!s) return;
+
+    let counter = 0;
+    for (let i = 0; i < s.length - 1; i++) { // stop loop one iteration before
+        let firstChar = i;
+        let secondChar = i + 1;
+        let neighborDiff = Math.abs(s.charCodeAt(firstChar) - s.charCodeAt(secondChar));
+        counter += neighborDiff;
+    }
+    return counter;
 };
+
+// // testing
+// console.log(scoreOfString("")) // undefined
+// console.log(scoreOfString("hello")) // 13
+// console.log(scoreOfString("zaz")) // 50
