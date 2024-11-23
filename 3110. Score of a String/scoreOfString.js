3269a8e904e2136d16acf450ed10cd5236e89e28
julesdmai <julesdmai@gmail.com>
2024-11-22 17:50:15 -0800

    Updated strategy

--- 3110. Score of a String/scoreOfString.js
+++ 3110. Score of a String/scoreOfString.js
@@ -5,7 +5,7 @@
 // Input: String
 // Output: A single number representing the accumulated value
 // O(n) // spaceO(1)
-// Strategy: Reduce method to accumulate a single value
+// Strategy: Reduce method to accumulate a single value // OR counter and loop
 var scoreOfString = function(s) {
     
 };
