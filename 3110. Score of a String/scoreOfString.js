b518a068154b6ebd6dca5d354b0955b41b81c88f
julesdmai <julesdmai@gmail.com>
2024-11-26 12:59:24 -0800

    Changed variable naming to be more descriptive

--- 3110. Score of a String/scoreOfString.js
+++ 3110. Score of a String/scoreOfString.js
@@ -5,18 +5,18 @@
 // Input: String
 // Output: A single number representing the accumulated value
 // O(n) // spaceO(1)
-// Strategy: Reduce method to accumulate a single value // OR counter variable and loop through string
+// Strategy: Reduce method to accumulate a single value // OR sumOfDiffs variable and loop through string
 var scoreOfString = function(s) {
     if (!s) return;
 
-    let counter = 0;
+    let sumOfDiffs = 0;
     for (let i = 0; i < s.length - 1; i++) { // stop loop one iteration before
         let firstIdx = i;
         let neighborIdx = i + 1;
         let neighborDiff = Math.abs(s.charCodeAt(firstIdx) - s.charCodeAt(neighborIdx));
-        counter += neighborDiff;
+        sumOfDiffs += neighborDiff;
     }
-    return counter;
+    return sumOfDiffs;
 };
 
 // // testing
