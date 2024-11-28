219f614d4b6fdb837610f916f25bd0a46db0030e
julesdmai <julesdmai@gmail.com>
2024-11-27 16:28:50 -0800

    countConsistentStrings wip

---
+++ 1684. Count the Number of Consistent Strings/countConsistentStrings.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} allowed
+ * @param {string[]} words
+ * @return {number}
+ */
+// Input: string of distinct chars
+// Output: array of strings representing words
+// O(n) // spaceO(1)
+// Strategy: Iterate through words, iterate through chars, increment counter if satisfies requirement
+var countConsistentStrings = function(allowed, words) {
+    if (!allowed || !words) return 0;
+
+    
+};
