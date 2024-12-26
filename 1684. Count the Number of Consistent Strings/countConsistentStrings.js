9d4e237176225ef4ce596a4adc2ad5badc4ef6b1
julesdmai <julesdmai@gmail.com>
2024-12-26 15:48:52 -0800

    Clarified inputs and outputs

--- 1684. Count the Number of Consistent Strings/countConsistentStrings.js
+++ 1684. Count the Number of Consistent Strings/countConsistentStrings.js
@@ -4,7 +4,8 @@
  * @return {number}
  */
 // Input: string of distinct chars
-// Output: array of strings representing words
+// Input2: array of strings representing words (criteria)
+// Output: Number representing words filtered through criteria
 // O(n) // spaceO(1)
 // Strategy: Iterate through words, iterate through chars, increment counter if satisfies requirement
 var countConsistentStrings = function(allowed, words) {
